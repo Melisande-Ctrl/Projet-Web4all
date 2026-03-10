1ae039b397090b395b1a09727708b845a8d1b8aa
Mélisande <melisande.coutarel@ikmail.com>
2026-03-10 11:42:18 +0100

    Reorganization

---
+++ arborescence/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/arborescence',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '0ee65c8dc4f458c3800c3b8efc005124c7fe96d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/arborescence' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '0ee65c8dc4f458c3800c3b8efc005124c7fe96d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
