f681493a234af1d24c3b074039d50461095d6403
Mélisande <melisande.coutarel@ikmail.com>
2026-03-13 17:01:13 +0100

    Composer related

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is the router, the main entry point of the application.
+ * It handles the routing and dispatches requests to the appropriate controller methods.
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInitfd0fee666d9c175c6e99dad6f265f7b4
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfd0fee666d9c175c6e99dad6f265f7b4::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
