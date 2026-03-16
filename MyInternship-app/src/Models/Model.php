2599a4e749077a1d671a181a19f4d5da51faebe4
Mélisande <melisande.coutarel@ikmail.com>
2026-03-16 12:16:16 +0100

    abstract classes Controller.php and Model.php created

---
+++ MyInternship-app/src/Models/Model.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+/**
+ * The Model class is an abstract class that serves as the base class for all models in the application.
+insert_record()
+puis controller
+route ds index.php*/
+abstract class Model {
+    protected $connection = null;
+}
