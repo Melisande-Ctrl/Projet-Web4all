553b995f1dce827d64301a6a9bd6ebc560e5376e
Melisande-Ctrl <melisande.coutarel@ikmail.com>
2026-03-23 14:46:17 +0100

    Refactor database connection in Model class
    Updated the connection property type and environment variable keys.

--- MyInternship-app/src/Models/Model.php
+++ MyInternship-app/src/Models/Model.php
@@ -7,12 +7,12 @@ use PDO, PDOException;
  * The Model class is a parent class that serves as the base class for all models in the application.
 */
 class Model {
-    protected $connection;
+    protected PDO $connection;
 
     public function __construct() {
         $env = parse_ini_file(".env");
         try {
-            $this->connection = new PDO('mysql:host=' . $env['servername'] . ';dbname='.$env['dbname'] . ';charset=utf8', $env['username'], $env['password']);
+            $this->connection = new PDO('mysql:host=' . $env['SERVERNAME'] . ';dbname='.$env['DBNAME'] . ';charset=utf8', $env['USERNAME'], $env['PASSWORD']);
             // set the PDO error mode to exception
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
