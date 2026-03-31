90cb8a9272777b3c874d51ce22cd8bbd6c33e6db
thomas <tpgailler6@gmail.com>
2026-03-31 10:29:09 +0200

    Wishlist fonctionnelle

--- MyInternship-app/src/Controllers/EtudiantController.php
+++ MyInternship-app/src/Controllers/EtudiantController.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Models\EtudiantModel;
+use App\Models\WishlistModel;
 
 class EtudiantController extends Controleur {
 
     private EtudiantModel $etudiantModel;
+    private WishlistModel $wishlistModel;
 
     public function __construct($twig){
         parent::__construct($twig);
         $this->etudiantModel = new etudiantModel();
+        $this->wishlistModel = new WishlistModel();
     }
 
     public function showDashboard(): void
@@ -33,10 +36,19 @@ class EtudiantController extends Controleur {
             'wishlist' => 'Ma Wishlist'
         ];
 
+        $wishlist = [];
+
+        if ($section === 'wishlist') {
+            $wishlist = $this->wishlistModel->getWishlistByUser($_SESSION['user']['id']);
+        }
+
+
+
         $this->render('dashboard/MonCompteEtudiant.html.twig', [
             'section' => $section,
             'menu' => $menu,
-            'route' => 'etudiant_dashboard'
+            'route' => 'etudiant_dashboard',
+            'wishlist' => $wishlist
         ]);
     }
 }
